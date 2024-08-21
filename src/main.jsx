1944706c3fb3a3bcc1aa4761a506111a327955eb
John Mark Fuentes <jmcaluyafuentes@gmail.com>
2024-08-21 23:03:50 +1000

    Implement initial PreviewRecipes page

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx'
+import Home from './pages/Home.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <StrictMode>
   <BrowserRouter>
+    <Home />
     <App />
   </BrowserRouter>
   // </StrictMode>,
