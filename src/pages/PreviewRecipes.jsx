9933efbe22d2185c11d22324bb402272f5687e2b
John Mark Fuentes <jmcaluyafuentes@gmail.com>
2024-08-22 15:13:37 +1000

    Added nutritional info in previewed recipes

--- src/pages/PreviewRecipes.jsx
+++ src/pages/PreviewRecipes.jsx
@@ -15,37 +15,59 @@ const PreviewRecipes = ({ recipes }) => {
 
     return (
       <>
-        <div className="container">
-          <div className="columns is-multiline">
-              {recipes.map(recipe => (
-                  <div key={recipe.id} className="column is-one-third">
-                      <div className="recipe card">
-                          <div className="card-image">
-                              <figure className="image">
-                                  <img src={recipe.image} alt={recipe.title} />
-                              </figure>
-                          </div>
-                          <div className="card-content">
-                              <div className="media">
-                                  <div className="media-content">
-                                      <p className="title is-4">{recipe.title}</p>
-                                  </div>
+          <div className="container">
+              <div className="columns is-multiline">
+                  {recipes.map(recipe => (
+                      <div key={recipe.id} className="column is-one-third">
+                          <div className="recipe card">
+                              <div className="card-image">
+                                  <figure className="image">
+                                      <img src={recipe.image} alt={recipe.title} />
+                                  </figure>
                               </div>
-                              <div className="content">
-                                  {recipe.description}
+                              <div className="card-content">
+                                  <div className="media">
+                                      <div className="media-content">
+                                          <p className="title is-4">{recipe.title}</p>
+                                      </div>
+                                  </div>
+                                  <div className="content">
+                                      <h5 className="title is-5">Nutrition:</h5>
+                                      <p>Calories: {`${recipe.caloriesPerServing.toFixed(2)} kcal`}</p>
+                                      <p>Serving Size: {recipe.servingSize}</p>
+
+                                      <h5 className="title is-5">Diet and Health Information:</h5>
+                                      <p>
+                                          <strong>Diet Labels:</strong> {recipe.dietLabels.join(', ')}
+                                      </p>
+                                      <p>
+                                          <strong>Health Labels:</strong> {recipe.healthLabels.join(', ')}
+                                      </p>
+
+                                      <h5 className="title is-5">Ingredients:</h5>
+                                      <ul>
+                                          {recipe.ingredients.map((ingredient, index) => (
+                                              <li key={index}>{ingredient}</li>
+                                          ))}
+                                      </ul>
+
+                                      <h5 className="title is-5">Preparation:</h5>
+                                      <a href={recipe.instructionsUrl} className="button is-primary ml-4">
+                                          See instructions
+                                      </a>
+                                      <p className="mt-2 ml-4">Source: <a href={recipe.instructionsUrl} target="_blank" rel="noopener noreferrer" className="has-text-info"><em>{recipe.source}</em></a></p>
+                                  </div>
                               </div>
                           </div>
                       </div>
-                  </div>
-              ))}
+                  ))}
+              </div>
           </div>
-          
-        </div>
-        <div className="mt-6 pt-6">
-            <Footer />
-        </div>
+          <div className="mt-6 pt-6">
+              <Footer />
+          </div>
       </>
-    );
+  );
 };
 
 export default PreviewRecipes;
