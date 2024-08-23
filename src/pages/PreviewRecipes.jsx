f41b40104b9f810bfaf8e0d97560c34573208ba0
John Mark Fuentes <jmcaluyafuentes@gmail.com>
2024-08-23 22:43:20 +1000

    feat: add dish, meal and cuisine types in recipes card

--- src/pages/PreviewRecipes.jsx
+++ src/pages/PreviewRecipes.jsx
@@ -81,6 +81,12 @@ const PreviewRecipes = ({ recipes }) => {
                             {recipe.healthLabels.join(', ')}
                         </p>
 
+                        <h5 className="title is-5">Dish Classification:</h5>
+                        <p> Dish Type: {recipe.dishType.map(dish => dish.charAt(0).toUpperCase() + dish.slice(1)).join(', ')}<br />
+                            Meal Type: {recipe.mealType.map(meal => meal.charAt(0).toUpperCase() + meal.slice(1)).join(", ")}<br />
+                            Cuisine: {recipe.cuisineType.map(cuisine => cuisine.charAt(0).toUpperCase() + cuisine.slice(1)).join(', ')}<br />
+                        </p>
+
                         <h5 className="title is-5">Ingredients:</h5>
                         <ul>
                             {recipe.ingredients.map((ingredient, index) => (
