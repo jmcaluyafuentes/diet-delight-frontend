819548a0936eddccca91cdd4a689de86d411a6e5
John Mark Fuentes <jmcaluyafuentes@gmail.com>
2024-08-23 23:30:26 +1000

    chore: change background color of image

--- src/components/HomePreview.jsx
+++ src/components/HomePreview.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import './HomePreview.css'
 
 const dietOptions = ['balanced', 'high-fiber', 'high-protein', 'low-carb', 'low-fat', 'low-sodium'];
 const healthOptions = ['dairy-free', 'egg-free', 'gluten-free', 'low-potasium', 'low-sugar', 'Mediterranean', 'mustard-free', 'no-oil-added', 'soy-free', 'sugar-conscious', 'tree-nut-free', 'vegan', 'vegetarian', 'wheat-free'];
@@ -47,7 +48,7 @@ const RecipeDisplay = () => {
                         <div className="recipe card">
                                 <div className="card-image">
                                     <figure className="image">
-                                        <a href={recipe.instructionsUrl} className="button is-primary ml-4">
+                                        <a href={recipe.instructionsUrl} className="button ml-2">
                                             <img src={recipe.image} alt={recipe.title} /> 
                                         </a>
                                     </figure>
