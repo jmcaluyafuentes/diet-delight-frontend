9afb087902fe19c6aa62b7e8658a5f4fe7f913d7
John Mark Fuentes <jmcaluyafuentes@gmail.com>
2024-08-31 09:33:00 +1000

    chore(FeaturedRecipes): added comments

--- src/components/FeaturedRecipes.jsx
+++ src/components/FeaturedRecipes.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
 import LoadingSpinner from './LoadingSpinner';
 import { dietOptions, healthOptions } from '../utils/dietHealthOptions.js';
 import { fetchRecipes } from '../utils/fetchRecipes.js';
@@ -12,6 +11,12 @@ import AddToPrintButton from './AddToPrintButton.jsx';
 import PrintToPDFButton from './PrintToPDFButton.jsx';
 import './FeaturedRecipes.css';
 
+// Utility functions outside the component to prevent recreation on every render
+// Get random element from an array as reference in featuring the recipes
+const getRandomElement = (array) => array[Math.floor(Math.random() * array.length)];
+// Shuffle the elements of an array randomly
+const shuffleArray = (array) => array.sort(() => Math.random() - 0.5);
+
 const FeaturedRecipes = () => {
     const [allRecipes, setAllRecipes] = useState([]);
     const [displayedRecipes, setDisplayedRecipes] = useState([]);
@@ -19,58 +24,62 @@ const FeaturedRecipes = () => {
     const [randomDiet, setRandomDiet] = useState('');
     const [isLoading, setIsLoading] = useState(false);
     const [isShuffling, setIsShuffling] = useState(true);
-    const navigate = useNavigate();
 
-    // Get random element as reference in featuring the recipes
-    const getRandomElement = (array) => array[Math.floor(Math.random() * array.length)];
-    // Shuffle the array
-    const shuffleArray = (array) => array.sort(() => Math.random() - 0.5);
-
+    // Fetch recipes when the component mounts
     useEffect(() => {
+            // Select random diet and health options to filter recipes
             const selectedDiet = getRandomElement(dietOptions);
             const randomHealth = getRandomElement(healthOptions);
-
             setRandomDiet(selectedDiet);
 
-            // Convert selectedDiet and randomHealth to arrays
+            // Convert selectedDiet and randomHealth to arrays for the fetch function
             const selectedDietArray = [selectedDiet];
             const randomHealthArray = [randomHealth];
 
+            // Fetch recipes based on random diet and health options
             fetchRecipes(selectedDietArray, randomHealthArray, setAllRecipes, setIsLoading);
     }, []);
 
+    // Set the initial set of displayed recipes after fetching
     useEffect(() => {
         if (allRecipes.length > 0) {
+            // Show only the first 4 recipes initially
             setDisplayedRecipes(allRecipes.slice(0, 4));
         }
     }, [allRecipes]);
 
-    // Shuffle the recipes every 5 seconds and select 4 recipes for display
+    // Shuffle the recipes every 5 seconds and update the displayed recipes
     useEffect(() => {
         let intervalId;
         if (isShuffling) {
+            // Set up an interval to shuffle the recipes every 5 seconds
             intervalId = setInterval(() => {
                 if (allRecipes.length > 0) {
                     const shuffled = shuffleArray([...allRecipes]);
+                    // Update the displayed recipes with the shuffled selection
                     setDisplayedRecipes(shuffled.slice(0, 4));
                 }
-            }, 5000); // 5 seconds
+            }, 5000); // 5 seconds interval
         }
 
+        // Cleanup function to clear the interval when the component unmounts or when shuffling stops
         return () => clearInterval(intervalId);
     }, [allRecipes, isShuffling]);
 
     return (
         <div className="recipe-display">
             {isLoading ? (
+                // Show loading spinner while recipes are being fetched
                 <LoadingSpinner />
             ) : (
                 <>
+                    {/* Display the featured recipes title based on the random diet selected */}
                     <h2 className="title is-3 has-text-centered mt-5" id="homepagefeaturedrecipetitle">
                         Featured Recipes for a {randomDiet} Diet
                     </h2>
 
                     <div className="columns is-multiline is-centered">
+                        {/* Render each recipe in a responsive column layout */}
                         {displayedRecipes.map(recipe => (
                             <div key={recipe.instructionsUrl} className="column is-one-quarter-desktop is-half-tablet is-full-mobile">
                                 <div className="recipe card">
@@ -101,7 +110,7 @@ const FeaturedRecipes = () => {
                                         </div>
 
                                         <div className="has-text-centered mt-3">
-                                            {/* AddToPrintButton component */}
+                                            {/* Button to add or remove recipes from the print list */}
                                             <AddToPrintButton
                                                 recipe={recipe}
                                                 setSelectedRecipes={setSelectedRecipes}
@@ -114,6 +123,7 @@ const FeaturedRecipes = () => {
                             </div>
                         ))}
                     </div>
+                    {/* Conditionally render the 'Print to PDF' button if there are selected recipes */}
                     {selectedRecipes.size > 0 && (
                         <div className="has-text-centered mt-4">
                             <PrintToPDFButton
